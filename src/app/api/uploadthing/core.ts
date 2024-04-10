c01738b34c0ec25ef874f032e07aedb6a1aacb88
rornsa <rysarorn8@gmail.com>
2024-04-10 12:54:12 +0700

    sam

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,7 +3,9 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 const f = createUploadthing();
 
 export const ourFileRouter = {
+  // @ts-ignore
   imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 10 } })
+  // @ts-ignore
     .onUploadComplete(async ({ metadata, file }) => {
       return { file };
     }),
