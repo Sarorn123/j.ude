5101aaec42303be052f5819326ddf445caa5ce3b
hanjery <hanjery5@gmail.com>
2024-08-16 12:22:43 +0700

    fixed

--- src/app/lib/env.ts
+++ src/app/lib/env.ts
@@ -3,12 +3,10 @@ import { z } from "zod";
 
 export const env = createEnv({
     server: {
-        // DATABASE_URL: z.string().url(),
+        DATABASE_URL: z.string().url(),
         GOOGLE_CLIENT_SECRET: z.string(),
         GOOGLE_CLIENT_ID: z.string(),
         HOST_NAME: z.string().url(),
-        POSTGRES_PRISMA_URL: z.string().url(),
-        POSTGRES_URL_NON_POOLING: z.string().url(),
     },
     experimental__runtimeEnv: process.env
 });
